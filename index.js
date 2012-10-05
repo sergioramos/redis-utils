84b1e57c8cb358fa281183a3c9dc3393443d145c
Sérgio Ramos <mail@sergioramos.me>
2012-10-06 00:35:45 +0100

    api changed and improved

--- index.js
+++ index.js
@@ -1,63 +1,116 @@
-var spawn = require('child_process').spawn,
+var rediis = require('redis-node'),
+    cp = require('child_process'),
     regex = require('./regex'),
-    redis = require('redis')
+    redis = require('redis'),
+    fs = require('fs')
 
-module.exports = function (config, callback) {
+var defaults = {
+  port: '6379',
+  host: '127.0.0.1'
+}
+
+var ensure = function (config, args) {
+  if(args && config.conf) args.push(config.conf)
+  if(!config.host) config.host = defaults.host
+  if(!config.port) config.port = defaults.port
+
+  return args
+}
+
+var running = function (config, callback) {
+  var client = rediis.createClient(config.port, config.host)
+
+  if(config.password) client.auth(config.password)
+
+  client.on('connected', function () {
+    client.close()
+    callback(null, true)
+  })
+
+  client.on('connection error', function () {
+    client.close()
+    callback(null, false)
+  })
+}
+
+var parse = function (config, callback) {
+  var onFile = function (e, data) {
+    if(e) throw e
+
+    var port = data.match(regex.port)
+    var host = data.match(regex.host)
+
+    if(port) config.port = port[1]
+    if(host) config.host = host[1]
+
+    callback()
+  }
+
+  fs.readFile(config.conf, 'utf8', onFile)
+}
+
+var spawn = function (args, callback) {
+  var rs = cp.spawn('redis-server', args)
+
+  rs.stderr.on('data', function (e) {
+    callback(new Error(e.toString()))
+  })
+
+  rs.stdout.on('data', function (data) {
+    if(data.toString().match(regex)) callback(null)
+  })
+
+  process.on('exit', rs.kill)
+}
+
+module.exports.server = function (config, callback) {
+  var rs
+
+  if(!config) config = {}
+  var args = ensure(config, [])
+
+  var onRunning = function (e, running) {
+    if(running) return callback()
+    spawn(args, callback)
+  }
+
+  var onConfig = function () {
+    running(config, onRunning)
+  }
+
+  if(args.length > 0) return parse(config, onConfig)
+  onConfig()
+}
+
+module.exports.client = function (config, callback) {
   var state = {
-    running: false,
     ready: false,
     auth: false
   }
-  
-  var args = [],
-      client, rs
-  
+
   var check = function () {
-    if(config.auth && (state.running && state.ready && state.auth)) callback()
-    else if(state.running && state.ready) callback()
+    if(config.auth && (state.ready && state.auth)) callback()
+    else if(state.ready) callback()
   }
-  
-  var kill = function () {
-    rs.kill()
-    client.end()
-  }
-  
+
   if(!config) config = {}
-  if(config.conf) args.push(config.conf)
-  
-  rs = spawn('redis-server', args)
-  client = redis.createClient(config.host || null, config.port || null)
-  
-  client.kill = kill
-  
+  ensure(config)
+
+  var client = redis.createClient(config.port, config.host)
+
   if(config.auth) client.auth(config.auth.pass, function () {
     state.auth = true
     check()
   })
-  
+
   client.on('ready', function () {
     state.ready = true
     check()
   })
-  
+
   client.on('error', function (e) {
     callback(e)
   })
-  
-  rs.stderr.on('data', function (e) {
-    console.log('dataerror', e);
-    callback(new Error(e.toString()))
-  })
-  
-  rs.stdout.on('data', function (data) {
-    if(data.toString().match(regex)) {
-      state.running = true
-      check()
-    }
-  })
-  
-  process.on('exit', kill)
-    
+
   return client
 }
-
--- regex.js
+++ regex.js
@@ -1,1 +1,3 @@
-module.exports = /\[\d*\] \d{2} .*? \d{2}:\d{2}:\d{2} \* The server is now ready to accept connections on port \d*/
+module.exports.running = /\[\d*\] \d{2} .*? \d{2}:\d{2}:\d{2} \* The server is now ready to accept connections on port \d*/
+module.exports.host = /^bind (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/im
+module.exports.port = /^port (\d{1,5})$/im
